22b673c698529a774037c6daca547e4f6fe86106
Sandeep Saldanha <supersan83@gmail.com>
2015-04-03 16:01:47 -0400

    Couple of UI Changes to the app
    Changed the action bar color and stuff

--- app/src/main/java/habitatfinal/habitatfinal/dashboard.java
+++ app/src/main/java/habitatfinal/habitatfinal/dashboard.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 
 public class dashboard extends Activity {
 
+
     private GLSurfaceView mGLSurface;
 
     @Override
@@ -39,5 +40,28 @@ public class dashboard extends Activity {
         // If you de-allocated graphic objects for onPause()
         // this is a good place to re-allocate them.
         mGLSurface.onResume();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_dashboard, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.action_dashboard:
+                return true;
+            case R.id.action_profile:
+                Intent h = new Intent(this, profile.class);
+                startActivity(h);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
