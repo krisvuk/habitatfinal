df9f6474117fff630ec5573b0b04fbb8470f3005
Sandeep Saldanha <supersan83@gmail.com>
2015-04-02 22:23:21 -0400

    Added OpenGL Earth model to the application
    The application has an earth

--- app/src/main/java/habitatfinal/habitatfinal/dashboard.java
+++ app/src/main/java/habitatfinal/habitatfinal/dashboard.java
@@ -2,6 +2,7 @@ package habitatfinal.habitatfinal;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -9,34 +10,34 @@ import android.view.MenuItem;
 
 public class dashboard extends Activity {
 
+    private GLSurfaceView mGLSurface;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
-    }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_dashboard, menu);
-        return true;
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity
+        mGLSurface = new EarthGLSurfaceView(this);
+        setContentView(mGLSurface);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId()) {
-            case R.id.action_dashboard:
-                return true;
-            case R.id.action_profile:
-                Intent h = new Intent(this, profile.class);
-                startActivity(h);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
+    protected void onPause() {
+        super.onPause();
+        // The following call pauses the rendering thread.
+        // If your OpenGL application is memory intensive,
+        // you should consider de-allocating objects that
+        // consume significant memory here.
+        mGLSurface.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // The following call resumes a paused rendering thread.
+        // If you de-allocated graphic objects for onPause()
+        // this is a good place to re-allocate them.
+        mGLSurface.onResume();
     }
 }
